71fee3c879f844976f851af6f1baa84618df3a6d
zheyuanw <88601021+zheyuanw@users.noreply.github.com>
2022-10-22 17:32:33 +1100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    //disableHostCheck: "true",
+    disableHostCheck: true,
     proxy: {
       '/api': {
         target: 'https://moivehub-itproject-team004.herokuapp.com',
